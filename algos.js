50edfde6040b5b44f6f42225467cbe2ea87562ae
Danyal <danyal@danyals-mbp.attlocal.net>
2017-11-07 12:54:22 -0600

    string unique

---
+++ algos.js
@@ -0,0 +1,17 @@
+// Reverse a string 
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+reverseString("hello");
+
+//  Are all the characters in a string unique
+// doesn't work for spaces
+var isU = function(str){
+    for(i=0; i<str.length; i++){
+        for(j=i+1; j<str.length; j++){
+            if(str.charAt(i) === str.charAt(j) && str.charAt(i) != " "){return false}
+        }
+    }
+    return true
+}
